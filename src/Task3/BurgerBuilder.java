4486be956f59d1037394841e82b0678566a04dc6
Asif032 <asif01981872223@gmail.com>
2023-08-08 19:48:23 +0600

    Task3 done

---
+++ src/Task3/BurgerBuilder.java
@@ -0,0 +1,33 @@
+package Task3;
+
+public class BurgerBuilder {
+  private Burger burger;
+
+  public BurgerBuilder(String burgerType) {
+    burger = new Burger(burgerType);
+  }
+
+  public BurgerBuilder addLettuce() {
+    burger.addLettuce();
+    return this;
+  }
+
+  public BurgerBuilder addTomato() {
+    burger.addTomato();
+    return this;
+  }
+
+  public BurgerBuilder addCheese() {
+    burger.addCheese();
+    return this;
+  }
+
+  public BurgerBuilder addOnion() {
+    burger.addOnion();
+    return this;
+  }
+
+  public Burger build() {
+    return burger;
+  }
+}
--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -1,4 +1,15 @@
 package Task3;
 
 public class Main {
+  public static void main(String[] args) {
+    BurgerBuilder builder = new BurgerBuilder("Classic Burger");
+
+    Burger burger = builder
+            .addLettuce()
+            .addTomato()
+            .addCheese()
+            .build();
+
+    System.out.println(burger.getDescription());
+  }
 }
